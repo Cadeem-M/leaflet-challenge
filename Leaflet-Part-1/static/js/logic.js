e395b81a6e92a9c73ace6c6df3487574b83a021a
SmallCad <160338891+SmallCad@users.noreply.github.com>
2024-06-15 00:39:56 -0400

    figured how to get map and pop ups to work. now i have to turn blue pointers into custom circle markers.

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,58 +1,82 @@
-//mapMark is a function that takes a list of cordinates and adds pop ups
-//  to "map" at those coordinates
-
-function mapMark(emptyList, geolist, depthList, nameList, magnitudeList) {
-    for (let i in geolist) {
-       let eqmarker =  L.marker([geolist[i][1], geolist[i][0]]).bindPopup(`<b>${nameList[i]}
-        </b><br> Earthquake Magnitude: ${magnitudeList[i]}`);
-
-       emptyList.push(eqmarker);
-    }
-
-    L.control.layers(emptyList).addTo(map);
+// Perform a GET request to the query URL/
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson").then(function (data) {
+    // Once we get a response, send the data.features object to the createFeatures function.
+    createFeatures(data.features);
+  });
+  
+  function createFeatures(earthquakeData) {
+  
+    // Define a function that we want to run once for each feature in the features array.
+    // Give each feature a popup that describes the place and magnitude of the earthquake.
+    function onEachFeature(feature, layer) {
+          
+      let geojsonFeature = {
+        "type": "Feature",
+        "properties": {
+            "name": `${feature.properties.place}`,
+            "popupContent": `<h3>${feature.properties.place}</h3><hr><p> Earthquake Magnitude: ${feature.properties.mag}</p>`
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [feature.geometry.coordinates[0], feature.geometry.coordinates[1]]
+        }
+      };
+      layer.bindPopup(geojsonFeature.properties.popupContent);
     
-};
+    };
 
 
-// Retrieving earthquake information of magnitutude 4.5+ over the last month. 
-function CreateMarkers(response) {
-        //Retrieving list of earthquakes from the data
-        let quakesList = response.features;
+ 
+    
 
-        //Creating lists to hold cordinates, earthquake depth, name of location earthquake happened,
-        //   and magnitude of the earthquake 
-        let quakeCoords = [];
-        let depthCoords = [];
-        let locateList = [];
-        let magList = [];
+ 
+    // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+    // Run the onEachFeature function once for each piece of data in the array.
+    let earthquakes = L.geoJSON(earthquakeData, {
+      onEachFeature: onEachFeature
+    });
 
-        //Looping through quakesList and storing geo cordinates in 
-        for (let quake of quakesList) {
-            quakeCoords.push([quake.geometry.coordinates[0],
-                             quake.geometry.coordinates[1]]);
-            depthCoords.push(quake.geometry.coordinates[2]);
-            locateList.push(quake.properties.place);
-            magList.push(quake.properties.mag);
-        };
+  
+    // Send our earthquakes layer to the createMap function/
+    createMap(earthquakes);
+  };
 
-        // initializing map base using first data entry as set view
-        let map = L.map("map").setView([quakeCoords[0][1],quakeCoords[0][0]], 4);
-
-        L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-	    attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-	    maxZoom: 16
-        }).addTo(map);
-
-        // Creating an empty list to hold layers
-        layers = []
-
-        mapMark(layers, quakeCoords, depthCoords, locateList, magList);
-    }
-);
-       
-
-// Perform an API call to the Citi Bike API to get the station information. Call createMarkers when it completes
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson").then(CreateMarkers);
+  
+  function createMap(earthquakes) {
+  
+    // Create the base layers.
+    let topo = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+    });
+  
+    
+    // Create a baseMaps object.
+    let baseMaps = {
+      "Topo Map": topo,
+    };
+  
+    // Create an overlay object to hold our overlay.
+    let overlayMaps = {
+      Earthquakes: earthquakes
+    };
+  
+    // Create our map, giving it the topo map and earthquakes layers to display on load.
+    let myMap = L.map("map", {
+      center: [
+        37.09, -95.71
+      ],
+      zoom: 6,
+      layers: [topo, earthquakes]
+    });
+  
+    // Create a layer control.
+    // Pass it our baseMaps and overlayMaps.
+    // Add the layer control to the map.
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(myMap);
+  
+  }
 
 
     
